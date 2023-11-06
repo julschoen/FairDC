e53411942b2796dfe17b86e517e5a97e698e78d8
julschoen <julianschoen@gmx.de>
2023-11-06 15:25:53 +0100

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 import torch
 from dataset import *
+import matplotlib.pyplot as plt
 
 train = get_train_loader(128)
 
@@ -7,4 +8,6 @@ for x,y in train:
 	print(x.shape)
 	print(y[0])
 
+	plt.imshow((x[0].permute(1,2,0).cpu().numpy()+1)/2)
+	plt.savefig('../im.png')
 	break
