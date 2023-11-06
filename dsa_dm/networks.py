b4f2b51ecf04ce26d0d6782338c4016356f45b56
julschoen <julianschoen@gmx.de>
2023-11-06 17:12:44 +0100

    Update networks.py

--- dsa_dm/networks.py
+++ dsa_dm/networks.py
@@ -44,6 +44,7 @@ class ConvNet(nn.Module):
     def forward(self, x):
         out = self.features(x)
         out = out.view(out.size(0), -1)
+        print(out.shape)
         out = self.classifier(out)
         return out
 
