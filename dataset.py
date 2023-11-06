3240ab1e59839f1fcd7797ad1e9a332ab6e6dba5
julschoen <julianschoen@gmx.de>
2023-11-06 15:11:48 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,7 +5,6 @@ from torch.utils.data import Dataset
 
 
 class MNIST(Dataset):
-    """Face Landmarks dataset."""
     def __init__(self, train=True, transform=None):
         self.train_dataset = datasets.MNIST(
             root="../data",
@@ -14,15 +13,36 @@ class MNIST(Dataset):
             transform=transform,
         )
 
+        self.color_map = {
+        0: (255, 0, 0),   # Red
+        1: (0, 255, 0),   # Green
+        2: (0, 0, 255),   # Blue
+        3: (255, 255, 0), # Yellow
+        4: (255, 0, 255), # Magenta
+        5: (0, 255, 255), # Cyan
+        6: (192, 192, 192), # Silver
+        7: (128, 0, 128), # Purple
+        8: (128, 128, 0), # Olive
+        9: (0, 128, 128), # Teal
+    }
+
+    def convert(self, image, label):
+        image = image.convert("RGB")  # Convert grayscale to RGB
+        # Apply the color corresponding to the class label
+        r, g, b = self.color_map[label]
+        colored_image = transforms.functional.adjust_hue(image, r/255.0)
+        colored_image = transforms.functional.adjust_saturation(colored_image, g/255.0)
+        colored_image = transforms.functional.adjust_brightness(colored_image, b/255.0)
+        return colored_image, label
+
     def __len__(self):
         return self.train_dataset.__len__()
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
         x,y = self.train_dataset.__getitem__(idx)
-        return x.repeat(3,1,1),y
+        return self.convert(x,y)
 
 
 def get_train_loader(batch_size):
