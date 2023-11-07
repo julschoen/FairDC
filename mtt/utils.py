c4e15b7a45508c3d839fd457f1278e2a6db0c43e
julschoen <julianschoen@gmx.de>
2023-11-07 15:29:31 +0100

    Update utils.py

--- mtt/utils.py
+++ mtt/utils.py
@@ -14,20 +14,68 @@ from torchvision import datasets, transforms
 from scipy.ndimage.interpolation import rotate as scipyrotate
 from networks import MLP, ConvNet, LeNet, AlexNet, VGG11BN, VGG11, ResNet18, ResNet18BN_AP, ResNet18_AP
 
-class CelebA(Dataset):
-    """Face Landmarks dataset."""
-    def __init__(self, split='train', transform=None, attributes=['Blond_Hair']):
-        self.train_dataset = datasets.CelebA(
+class MNIST(Dataset):
+    def __init__(self, train=True, transform=None, majority=0.5):
+        self.train_dataset = datasets.MNIST(
             root="../../data",
-            split=split,
-            download=False,
+            train=train,
+            download=True,
             transform=transform,
         )
 
-        self.classes = attributes
-        self.target_inds = []
-        for attr in self.classes:
-            self.target_inds.append(self.train_dataset.attr_names.index(attr))
+        self.color_map = {
+            0: (255., 0., 0.),   # Red
+            1: (0, 255, 0),   # Green
+            2: (0, 0, 255),   # Blue
+            3: (255, 255, 0), # Yellow
+            4: (255, 0, 255), # Magenta
+            5: (0, 255, 255), # Cyan
+            6: (192, 192, 192), # Silver
+            7: (194, 125, 25), # Orange
+            8: (190., 217., 190.), # Bright Olive
+            9: (255., 255., 255.), # White
+        }
+
+        self.majority_percentage = majority
+
+        # Precompute the indices for the majority and minority splits
+        self.index_color_map = self._precompute_indices()
+
+    def _precompute_indices(self):
+        # Determine the number of samples per class
+        targets = self.train_dataset.targets.numpy()
+        num_samples = len(targets)
+        indices = np.arange(num_samples)
+
+        index_color_map = {}
+        for class_label, class_color in self.color_map.items():
+            print(class_label, class_color)
+            class_indices = indices[targets == class_label]
+            np.random.shuffle(class_indices)
+
+            # Compute the split index for majority/minority
+            split_idx = int(len(class_indices) * self.majority_percentage)
+
+            # Assign the majority color
+            for idx in class_indices[:split_idx]:
+                index_color_map[idx] = class_color
+
+            # Assign the minority colors uniformly from other classes
+            other_colors = [color for label, color in self.color_map.items() if label != class_label]
+            for idx in class_indices[split_idx:]:
+                index_color_map[idx] = other_colors[np.random.randint(len(other_colors))]
+
+        return index_color_map
+
+    def convert(self, image, id):
+        rgb_image = image.clone().repeat(3,1,1)
+        r,g,b = self.index_color_map[id]
+        r,g,b = r/255.0, g/255.0, b/255.0
+        image = (image+1)/2
+        rgb_image[0] = (image*r*2)-1
+        rgb_image[1] = (image*g*2)-1
+        rgb_image[2] = (image*b*2)-1
+        return rgb_image
 
     def __len__(self):
         return self.train_dataset.__len__()
@@ -35,10 +83,8 @@ class CelebA(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
         x,y = self.train_dataset.__getitem__(idx)
-
-        return x,y[self.target_inds]
+        return self.convert(x,idx),y
 
 class Config:
     imagenette = [0, 217, 482, 491, 497, 566, 569, 571, 574, 701]
@@ -75,7 +121,21 @@ def get_dataset(dataset, data_path, batch_size=1, subset="imagenette", args=None
     loader_train_dict = None
     class_map_inv = None
 
-    if dataset == 'CIFAR10':
+    if dataset == 'MNIST':
+        channel = 3
+        im_size = (28, 28)
+        num_classes = 10
+        mean = (0.5)
+        std = (0.5)
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        dst_train = MNIST(train=True, transform=transform) # no augmentation
+        dst_test = MNIST(train=False, transform=transform)
+        class_names = [str(c) for c in range(num_classes)]
+        class_map = {x:x for x in range(num_classes)}
+        mean = (0.5, 0.5, 0.5)
+        std = (0.5, 0.5, 0.5)
+
+    elif dataset == 'CIFAR10':
         channel = 3
         im_size = (32, 32)
         num_classes = 10
