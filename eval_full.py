5f7164fb5e24e55236adff202813ed0bf5b6f817
julschoen <julianschoen@gmx.de>
2023-11-08 12:56:21 +0100

    Update eval_full.py

--- eval_full.py
+++ eval_full.py
@@ -52,7 +52,7 @@ def main():
     print('\n==================== Final Results ====================\n')
     for key in model_eval_pool:
         accs = accs_all_exps[key]
-        print('Evaluate %d random %s on %d SynSets, mean  = %.2f%%  std = %.2f%%'%(len(accs)/len(data), key, len(data), np.mean(accs)*100, np.std(accs)*100))
+        print('Evaluate %d random %s on Full, mean  = %.2f%%  std = %.2f%%'%(len(accs), key, np.mean(accs)*100, np.std(accs)*100))
 
     torch.save({
             'weights': model_weights,
