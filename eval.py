878513becd45b43db7ff9ff85453e9bc28dd810b
julschoen <julianschoen@gmx.de>
2023-11-08 10:50:01 +0100

    Eval

---
+++ eval.py
@@ -0,0 +1,62 @@
+import os
+import time
+import copy
+import glob
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision.utils import save_image
+from utils import get_dataset, get_network, get_eval_pool, evaluate_synset, get_daparam, match_loss, get_time, TensorDataset, epoch, DiffAugment, ParamDiffAug
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Parameter Processing')
+    parser.add_argument('--dataset', type=str, default='MNIST', help='dataset')
+    parser.add_argument('--num_eval', type=int, default=5, help='the number of evaluating randomly initialized models')
+    parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data')
+    parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
+    parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
+    parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
+    parser.add_argument('--cond_path', type=str, default='result', help='path to save results')
+
+    args = parser.parse_args()
+    args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.dsa_param = ParamDiffAug()
+    args.dsa = True
+    args.dsa_strategy ='color_crop_cutout_flip_scale_rotate'
+    
+    channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path)
+    model_eval_pool = get_eval_pool('M', None, None)
+
+    accs_all_exps = dict() # record performances of all experiments
+    for key in model_eval_pool:
+        accs_all_exps[key] = []
+
+    file = glob.glob(os.path.join(args.cond_path, '*.pt'))[-1]
+    image_syn, label_syn = torch.load(file)['data'][-1]
+    
+    for model_eval in model_eval_pool:
+        print('-------------------------\nEvaluation\nmodel_train = %s, model_eval = %s, iteration = %d'%(args.model, model_eval, it))
+        accs = []
+        for it_eval in range(args.num_eval):
+            net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device) # get a random model
+            image_syn_eval, label_syn_eval = copy.deepcopy(image_syn.detach()), copy.deepcopy(label_syn.detach()) # avoid any unaware modification
+            _, acc_train, acc_test = evaluate_synset(it_eval, net_eval, image_syn_eval, label_syn_eval, testloader, args)
+            accs.append(acc_test)
+        print('Evaluate %d random %s, mean = %.4f std = %.4f\n-------------------------'%(len(accs), model_eval, np.mean(accs), np.std(accs)))
+
+        accs_all_exps[model_eval] += accs
+
+
+    print('\n==================== Final Results ====================\n')
+    for key in model_eval_pool:
+        accs = accs_all_exps[key]
+        print('Run %d experiments, train on %s, evaluate %d random %s, mean  = %.2f%%  std = %.2f%%'%(args.num_exp, args.model, len(accs), key, np.mean(accs)*100, np.std(accs)*100))
+
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ utils.py
@@ -0,0 +1,38 @@
+def evaluate_synset(it_eval, net, images_train, labels_train, testloader, args, return_loss=False, texture=False):
+    net = net.to(args.device)
+    images_train = images_train.to(args.device)
+    labels_train = labels_train.to(args.device)
+    lr = float(args.lr_net)
+    Epoch = int(args.epoch_eval_train)
+    lr_schedule = [Epoch//2+1]
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
+
+    criterion = nn.CrossEntropyLoss().to(args.device)
+
+    dst_train = TensorDataset(images_train, labels_train)
+    trainloader = torch.utils.data.DataLoader(dst_train, batch_size=args.batch_train, shuffle=True, num_workers=0)
+
+    start = time.time()
+    acc_train_list = []
+    loss_train_list = []
+
+    for ep in tqdm.tqdm(range(Epoch+1)):
+        loss_train, acc_train = epoch('train', trainloader, net, optimizer, criterion, args, aug=True, texture=texture)
+        acc_train_list.append(acc_train)
+        loss_train_list.append(loss_train)
+        if ep == Epoch:
+            with torch.no_grad():
+                loss_test, acc_test = epoch('test', testloader, net, optimizer, criterion, args, aug=False)
+        if ep in lr_schedule:
+            lr *= 0.1
+            optimizer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
+
+
+    time_train = time.time() - start
+
+    print('%s Evaluate_%02d: epoch = %04d train time = %d s train loss = %.6f train acc = %.4f, test acc = %.4f' % (get_time(), it_eval, Epoch, int(time_train), loss_train, acc_train, acc_test))
+
+    if return_loss:
+        return net, acc_train_list, acc_test, loss_train_list, loss_test
+    else:
+        return net, acc_train_list, acc_test
