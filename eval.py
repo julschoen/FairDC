9324e0f1a209a9a8be3566fa77ee6e8c4f406030
julschoen <julianschoen@gmx.de>
2023-11-08 11:35:03 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -33,6 +33,10 @@ def main():
     for key in model_eval_pool:
         accs_all_exps[key] = []
 
+    model_weights = dict() # record performances of all experiments
+    for key in model_eval_pool:
+        model_weights[key] = []
+
     file = glob.glob(os.path.join(args.cond_path, '*.pt'))[-1]
 
     data = torch.load(file)['data']
@@ -41,20 +45,28 @@ def main():
 	    for model_eval in model_eval_pool:
 	        print('-------------------------\nEvaluation\nmodel_eval = %s'%(model_eval))
 	        accs = []
+	        weights = []
 	        for it_eval in range(args.num_eval):
 	            net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device) # get a random model
 	            image_syn_eval, label_syn_eval = copy.deepcopy(image_syn.detach()), copy.deepcopy(label_syn.detach()) # avoid any unaware modification
 	            _, acc_train, acc_test = evaluate_synset(it_eval, net_eval, image_syn_eval, label_syn_eval, testloader, args)
 	            accs.append(acc_test)
+	            weights.append(net_eval.state_dict())
 	        print('Evaluate %d random %s, mean = %.4f std = %.4f\n-------------------------'%(len(accs), model_eval, np.mean(accs), np.std(accs)))
 
 	        accs_all_exps[model_eval] += accs
+	        model_weights[model_eval] += weights
 
 
     print('\n==================== Final Results ====================\n')
     for key in model_eval_pool:
         accs = accs_all_exps[key]
         print('Evaluate %d random %s on %d SynSets, mean  = %.2f%%  std = %.2f%%'%(len(accs)/len(data), key, len(data), np.mean(accs)*100, np.std(accs)*100))
+
+    torch.save({
+            'weights': model_weights,
+            'accs': accs_all_exps
+        }, os.path.join(args.cond_path, 'eval.pt'))
 
 
 
