27de172d18d94cc873064d54f70784ae16ebb1f6
julschoen <julianschoen@gmx.de>
2023-11-10 13:04:27 +0100

    Update make_eval_set.py

--- make_eval_set.py
+++ make_eval_set.py
@@ -22,8 +22,8 @@ def main():
     data_save = []
 
     for run in os.listdir(args.cond_path):
-        image_syn = torch.load(os.path.join(args.cond_path, 'images_best.pt'))
-        label_syn = torch.load(os.path.join(args.cond_path, 'labels_best.pt'))
+        image_syn = torch.load(os.path.join(args.cond_path, run, 'images_best.pt'))
+        label_syn = torch.load(os.path.join(args.cond_path, run, 'labels_best.pt'))
         data_save.append([copy.deepcopy(image_syn.detach().cpu()), copy.deepcopy(label_syn.detach().cpu())])
     torch.save({'data': data_save, 'accs_all_exps': []}, os.path.join(args.cond_path, 'res_%s_%s_%dipc.pt'%(args.method, args.dataset, args.ipc)))
 
