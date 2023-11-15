b5c82c7f65fe365e1b090bc4543b03b2d744e102
julschoen <julianschoen@gmx.de>
2023-11-15 12:08:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,6 +382,26 @@ def get_network(model, channel, num_classes, im_size=(32, 32), dist=True):
 def get_time():
     return str(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()))
 
+def get_loops(ipc):
+    # Get the two hyper-parameters of outer-loop and inner-loop.
+    # The following values are empirically good.
+    if ipc == 1:
+        outer_loop, inner_loop = 1, 1
+    elif ipc == 10:
+        outer_loop, inner_loop = 10, 50
+    elif ipc == 20:
+        outer_loop, inner_loop = 20, 25
+    elif ipc == 30:
+        outer_loop, inner_loop = 30, 20
+    elif ipc == 40:
+        outer_loop, inner_loop = 40, 15
+    elif ipc == 50:
+        outer_loop, inner_loop = 50, 10
+    else:
+        outer_loop, inner_loop = 0, 0
+        exit('loop hyper-parameters are not defined for %d ipc'%ipc)
+    return outer_loop, inner_loop
+
 def epoch(mode, dataloader, net, optimizer, criterion, args, aug, texture=False, full=False):
     loss_avg, acc_avg, num_exp = 0, 0, 0
     net = net.to(args.device)
