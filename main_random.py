44c1c3cba81d03beffb2e8887b4e64cb5fcc6db8
julschoen <julianschoen@gmx.de>
2023-11-21 13:44:42 +0100

    Update main_random.py

--- main_random.py
+++ main_random.py
@@ -34,7 +34,8 @@ def main():
     labels_all = torch.tensor(labels_all, dtype=torch.long)
 
     def get_images(c, n): # get random n images from class c
-        idx_shuffle = np.random.permutation(indices_class[c])[:n]
+        indices = indices_class[c]
+        idx_shuffle = np.random.permutation(indices)[:n]
         return images_all[idx_shuffle]
 
     for exp in range(args.num_exp):
