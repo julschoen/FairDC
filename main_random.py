a8679f3d61758ddae1bc7f58968a70a6fa8d4942
julschoen <julianschoen@gmx.de>
2023-11-21 09:47:50 +0100

    Update main_random.py

--- main_random.py
+++ main_random.py
@@ -12,6 +12,7 @@ def main():
     parser.add_argument('--ipc', type=int, default=10, help='image(s) per class')
     parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
     parser.add_argument('--save_path', type=str, default='random')
+    parser.add_argument('--attributes', type=str, default='Blond_Hair')
     args = parser.parse_args()
 
     if not os.path.exists(args.save_path):
