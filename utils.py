6a07d49ef5fbc0b696f476bad997d3bd77ba5932
julschoen <julianschoen@gmx.de>
2023-12-04 09:38:08 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import os
-import kornia as K
 import tqdm
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
