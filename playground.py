042f746faf7dd92f3d95d0b7bc2ce411cefc6ff6
julschoen <julianschoen@gmx.de>
2023-12-05 13:27:58 +0100

    Update playground.py

--- playground.py
+++ playground.py
@@ -11,6 +11,7 @@ parser.add_argument('--dataset', type=str, default='CelebA', help='dataset')
 parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
 parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
 parser.add_argument('--attributes', type=str, default='Blond_Hair')
+args = parser.parse_args()
 
 channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path, args=args)
 model_eval_pool = get_eval_pool(args.eval_mode, args.model, args.model)
