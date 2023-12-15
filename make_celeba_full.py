6fa98e86dac81315c97f52e0b52507098d753320
julschoen <julianschoen@gmx.de>
2023-12-15 02:26:00 +0100

    Create make_celeba_full.py

---
+++ make_celeba_full.py
@@ -0,0 +1,18 @@
+import torch
+
+model_weights = torch.load('celeba_full/eval.pt')['weights']
+conv_weights = torch.load('celeba_full/eval_conv.pt')['weights']
+
+accs_all_exps = torch.load('celeba_full/eval.pt')['accs']
+conv_accs = torch.load('celeba_full/eval_conv.pt')['accs']
+
+for key in conv_weights.keys():
+	model_weights[key] = conv_weights[key]
+
+for key in conv_accs.keys():
+	accs_all_exps[key] = conv_accs[key]
+
+torch.save({
+    'weights': model_weights,
+    'accs': accs_all_exps
+}, 'celeba_full/eval_50ipc.pt')
