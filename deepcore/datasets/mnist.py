aa4afadb0739d97b3b3a2d7a69914d67cd6721bb
julschoen <julianschoen@gmx.de>
2023-12-18 09:06:28 +0100

    Update mnist.py

--- deepcore/datasets/mnist.py
+++ deepcore/datasets/mnist.py
@@ -10,7 +10,7 @@ class MNIST_Dataset(Dataset):
             download=True,
             transform=transform,
         )
-        self.classes = 10
+        self.classes = [0,1,2,3,4,5,6,7,8,9]
         self.color_map = {
             1: (0., 0., 255.),   # Blue
             0: (255., 0., 0.) # Red
