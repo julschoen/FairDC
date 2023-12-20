277396cde3861d041225c70eefde6b533e9ecc4a
julschoen <julianschoen@gmx.de>
2023-12-20 11:41:46 +0100

    Update uniform.py

--- deepcore/methods/uniform.py
+++ deepcore/methods/uniform.py
@@ -3,11 +3,12 @@ from .coresetmethod import CoresetMethod
 
 
 class Uniform(CoresetMethod):
-    def __init__(self, dst_train, args, fraction=0.5, random_seed=None, balance=False, replace=False, **kwargs):
+    def __init__(self, dst_train, args, ipc, fraction=0.5, random_seed=None, balance=False, replace=False, **kwargs):
         super().__init__(dst_train, args, fraction, random_seed)
         self.balance = balance
         self.replace = replace
         self.n_train = len(dst_train)
+        self.ipc=ipc
 
     def select_balance(self):
         """The same sampling proportions were used in each class separately."""
@@ -17,13 +18,13 @@ class Uniform(CoresetMethod):
         for c in range(self.num_classes):
             c_index = (self.dst_train.targets == c)
             self.index = np.append(self.index,
-                                   np.random.choice(all_index[c_index], round(self.fraction * c_index.sum().item()),
+                                   np.random.choice(all_index[c_index], self.ipc,
                                                     replace=self.replace))
         return self.index
 
     def select_no_balance(self):
         np.random.seed(self.random_seed)
-        self.index = np.random.choice(np.arange(self.n_train), round(self.n_train * self.fraction),
+        self.index = np.random.choice(np.arange(self.n_train), self.ipc,
                                       replace=self.replace)
 
         return  self.index
