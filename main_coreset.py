4a8bb72a132a4d36a1483c35257c99e28e243d20
julschoen <julianschoen@gmx.de>
2023-12-22 10:38:26 +0100

    Update main_coreset.py

--- main_coreset.py
+++ main_coreset.py
@@ -21,45 +21,18 @@ def main():
     parser.add_argument('--model', type=str, default='ResNet18', help='model')
     parser.add_argument('--selection', type=str, default="uniform", help="selection method")
     parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
-    parser.add_argument('--num_eval', type=int, default=10, help='the number of evaluating randomly initialized models')
     parser.add_argument('--epochs', default=200, type=int, help='number of total epochs to run')
     parser.add_argument('--data_path', type=str, default='data', help='dataset path')
-    parser.add_argument('--gpu', default=None, nargs="+", type=int, help='GPU id to use')
+    parser.add_argument('--gpu', default=0, nargs="+", type=int, help='GPU id to use')
     parser.add_argument('--print_freq', '-p', default=20, type=int, help='print frequency (default: 20)')
     parser.add_argument('--ipc', type=int, default=10, help='Images per Class')
     parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
-    parser.add_argument("--cross", type=str, nargs="+", default=None, help="models for cross-architecture experiments")
-
-    # Optimizer and scheduler
-    parser.add_argument('--optimizer', default="SGD", help='optimizer to use, e.g. SGD, Adam')
-    parser.add_argument('--lr', type=float, default=0.1, help='learning rate for updating network parameters')
-    parser.add_argument('--min_lr', type=float, default=1e-4, help='minimum learning rate')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum (default: 0.9)')
-    parser.add_argument('-wd', '--weight_decay', default=5e-4, type=float,
-                        metavar='W', help='weight decay (default: 5e-4)',
-                        dest='weight_decay')
-    parser.add_argument("--nesterov", default=True, type=str_to_bool, help="if set nesterov")
-    parser.add_argument("--scheduler", default="CosineAnnealingLR", type=str, help=
-    "Learning rate scheduler")
-    parser.add_argument("--gamma", type=float, default=.5, help="Gamma value for StepLR")
-    parser.add_argument("--step_size", type=float, default=50, help="Step size for StepLR")
-
+    
     # Training
     parser.add_argument('--batch', '--batch-size', "-b", default=256, type=int, metavar='N',
                         help='mini-batch size (default: 256)')
-    parser.add_argument("--train_batch", "-tb", default=None, type=int,
-                     help="batch size for training, if not specified, it will equal to batch size in argument --batch")
-    parser.add_argument("--selection_batch", "-sb", default=None, type=int,
-                     help="batch size for selection, if not specified, it will equal to batch size in argument --batch")
-
-    # Testing
-    parser.add_argument("--test_interval", '-ti', default=1, type=int, help=
-    "the number of training epochs to be preformed between two test epochs; a value of 0 means no test will be run (default: 1)")
-    parser.add_argument("--test_fraction", '-tf', type=float, default=1.,
-                        help="proportion of test dataset used for evaluating the model (default: 1.)")
-
+    
     # Selecting
     parser.add_argument("--selection_epochs", "-se", default=40, type=int,
                         help="number of epochs whiling performing selection on full dataset")
@@ -143,7 +116,7 @@ def main():
         print('\n================== Exp %d ==================\n' % exp)
         print("dataset: ", args.dataset, ", model: ", args.model, ", selection: ", args.selection, ", num_ex: ",
               args.num_exp, ", epochs: ", args.epochs, ", IPC: ", args.ipc,
-              ", lr: ", args.lr, ", save_path: ", args.save_path, ", resume: ", args.resume, ", device: ", args.device,
+              ", save_path: ", args.save_path, ", resume: ", args.resume, ", device: ", args.device,
               ", checkpoint_name: " + checkpoint_name if args.save_path != "" else "", "\n", sep="")
 
         channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test = datasets.__dict__[args.dataset] \
