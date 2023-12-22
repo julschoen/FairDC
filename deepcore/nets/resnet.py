30ca63a2d662a62135e94cabdeb53124d4fe541b
julschoen <julianschoen@gmx.de>
2023-12-22 10:48:34 +0100

    Update resnet.py

--- deepcore/nets/resnet.py
+++ deepcore/nets/resnet.py
@@ -68,13 +68,13 @@ class Bottleneck(nn.Module):
         return out
 
 class ResNet_64x64(nn.Module):
-    def __init__(self, block, num_blocks, channel=3, num_classes=10, norm='instancenorm'):
+    def __init__(self, block, num_blocks, channel=3, num_classes=10, record_embedding: bool = False, no_grad: bool = False):
         super(ResNetImageNet, self).__init__()
         self.in_planes = 64
         self.norm = norm
 
         self.conv1 = nn.Conv2d(channel, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.GroupNorm(64, 64, affine=True) if self.norm == 'instancenorm' else nn.BatchNorm2d(64)
+        self.bn1 = nn.BatchNorm2d(64)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -82,6 +82,12 @@ class ResNet_64x64(nn.Module):
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Linear(512*block.expansion, num_classes)
+
+        self.embedding_recorder = EmbeddingRecorder(record_embedding)
+        self.no_grad = no_grad
+
+    def get_last_layer(self):
+        return self.classifier
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -92,17 +98,19 @@ class ResNet_64x64(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.maxpool(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        # out = F.avg_pool2d(out, 4)
-        # out = out.view(out.size(0), -1)
-        out = self.avgpool(out)
-        out = torch.flatten(out, 1)
-        out = self.classifier(out)
+        with set_grad_enabled(not self.no_grad):
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.maxpool(out)
+            out = self.layer1(out)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = self.layer4(out)
+            # out = F.avg_pool2d(out, 4)
+            # out = out.view(out.size(0), -1)
+            out = self.avgpool(out)
+            out = torch.flatten(out, 1)
+            out = self.embedding_recorder(out)
+            out = self.classifier(out)
         return out
 
 
