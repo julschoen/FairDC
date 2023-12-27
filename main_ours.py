d78b499a952bd4279ea09b832566b0d67edf14dc
julschoen <julianschoen@gmx.de>
2023-12-27 13:03:53 +0100

    Update main_ours.py

--- main_ours.py
+++ main_ours.py
@@ -29,7 +29,7 @@ def main():
     parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
     parser.add_argument('--epoch_eval_train', type=int, default=1000, help='epochs to train a model with synthetic data') # it can be small for speeding up with little performance drop
     parser.add_argument('--Iteration', type=int, default=20000, help='training iterations')
-    parser.add_argument('--lr_img', type=float, default=0.001, help='learning rate for updating synthetic images')
+    parser.add_argument('--lr_img', type=float, default=0.01, help='learning rate for updating synthetic images')
     parser.add_argument('--lr_net', type=float, default=0.01, help='learning rate for updating network parameters')
     parser.add_argument('--batch_real', type=int, default=256, help='batch size for real data')
     parser.add_argument('--batch_train', type=int, default=256, help='batch size for training networks')
