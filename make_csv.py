93d143eb89774dd1e5c15718bbbde4feff9d1404
julschoen <julianschoen@gmx.de>
2023-12-28 09:29:28 +0100

    Update make_csv.py

--- make_csv.py
+++ make_csv.py
@@ -71,6 +71,7 @@ def main():
             net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device)
             net_eval.load_state_dict(model_weights[model_eval][it_eval])
             pred, true, sf = evaluate_model(net_eval, testloader, args)
+            sf_nums = sf
             sf = sens_names[sf.astype('int')]
             metric_frame = MetricFrame(
                 metrics=metrics,
